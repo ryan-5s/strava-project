d5f53fbebd27ef8d1835e52f12b2d34515b5e8ef
Ryan Fives <ryanfives2006@gmail.com>
2026-06-03 11:28:15 -0400

    Update visualizing_runs.py

--- stravaProject/visualizing_runs.py
+++ stravaProject/visualizing_runs.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 #Pace vs. Temperature Graph
 runs_df = pd.read_csv('stravaProject/data/runs_with_weather.csv')
-runs_df.plot(kind='scatter', x='max_temperature', y='pace_min_per_mile')
+runs_df.plot(kind='scatter', x='start_temp', y='pace_min_per_mile')
 plt.title('Pace vs. Temperature')
 plt.xlabel('Temperature (degrees F)')
 plt.ylabel('Pace (min/mile)')
@@ -11,9 +11,9 @@ plt.show()
 
 #Pace vs. Temp vs. Humidty while accounting for length of the run
 runs_df.plot(kind='scatter',
-             x='max_temperature',
+             x='start_temp',
              y='pace_min_per_mile',
-             c='max_humidity',#color mapping
+             c='start_humidity',#color mapping
              cmap='YlOrRd',#color map scale
              s=runs_df['distance_miles'] * 25, #dot size based on distance of run
              alpha=0.6,
@@ -22,7 +22,7 @@ runs_df.plot(kind='scatter',
              figsize=(11, 7))
 
 plt.title('Pace vs. Heat, Humidity, & Distance')
-plt.xlabel('Max Temperature (degrees F)')
+plt.xlabel('Temperature (degrees F)')
 plt.ylabel('Pace (Minutes per Mile)')
 plt.grid(True, linestyle='--', alpha=0.4)
 
